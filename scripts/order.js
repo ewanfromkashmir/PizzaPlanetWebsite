6cf6ecea4df5c2b8e98d78273a83fa96aa7878f9
ewanhughes26 <79140539+ewanhughes26@users.noreply.github.com>
2023-01-15 23:42:56 +0000

    Fixed bug with pizza image not displaying on order.html page

--- scripts/order.js
+++ scripts/order.js
@@ -1,24 +1,50 @@
 window.addEventListener("DOMContentLoaded", function(e)
 {
+    // var locationBox = document.querySelector("#location");
+
+    // var location =
+    // {
+    //     latitude: unknown,
+    //     longitude: unknown
+    // };
+    
+    // window.navigator.geolocation.getCurrentPosition
+    // (
+    //     function(position)
+    //     {
+    //         location =
+    //         {
+    //             latitude: position.coords.latitude,
+    //             longitude: position.coords.longitude
+    //         };
+
+    //         locationBox.value = JSON.stringify(location);
+    //     },
+
+    //     function(error)
+    //     {
+    //         locationBox.value = JSON.stringify(location);
+    //     }
+    // );
+
     const order = localStorage.getItem("order");
+    const pizzaOrder = JSON.parse(order);
 
     if (order)
     {
-        const pizzaOrder = JSON.parse(order);
         const pizza = document.querySelector(".pizza");
-        const orderInput = document.querySelector("#pizza-order");
-        orderInput.value = order;
-
         const title = pizza.querySelector(".title");
         const price = pizza.querySelector(".price");
         const desc = pizza.querySelector(".desc");
+        const orderInput = document.querySelector("#pizza-order");
 
         title.innerText = pizzaOrder.title;
         price.innerText = pizzaOrder.price;
         desc.innerText = pizzaOrder.desc;
+        orderInput.value = order;
 
         const img = pizza.querySelector("img");
         img.setAttribute("src", `../images/pizzas/${pizzaOrder.id}.png`);
-        img.setAttribute("alt", pieOrder.title);
+        img.setAttribute("alt", pizzaOrder.title);
     }
 });

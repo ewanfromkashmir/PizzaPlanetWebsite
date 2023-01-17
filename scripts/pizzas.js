9ee2340916ad77a35140bdc7849bec5ef3dc68c2
ewanhughes26 <79140539+ewanhughes26@users.noreply.github.com>
2023-01-17 20:54:09 +0000

    Implemented total cost variable which is saved locally

--- scripts/pizzas.js
+++ scripts/pizzas.js
@@ -3,12 +3,14 @@ window.addEventListener("DOMContentLoaded", function(e)
     console.log("working")
     const orderButtons = document.querySelectorAll("button[data-order]");
 
+    var totalItems = 0;
+
     orderButtons.forEach(function(button)
     {
         button.addEventListener("click", function(e)
         {
             const button = e.currentTarget;
-            const container = button.parentNode;
+            const container = button.parentNode.parentNode;
             const order = 
             {
                 id: button.getAttribute("data-order"),
@@ -19,8 +21,28 @@ window.addEventListener("DOMContentLoaded", function(e)
 
             localStorage.setItem("order", JSON.stringify(order));
             const url = window.location.href.replace("pizzas.html", "order.html");
-            console.log(url);
-            window.location.href = url;
+            
+
+            addItemToBasket(order.price, button.getAttribute("data-order"));
+            //window.location.href = url;
         });
     });
+
+    var totalCost = 0.0;
+
+    function addItemToBasket(price, size)
+    {
+        var amount = parseFloat(Number(price.replace(/[^0-9.-]+/g,"")));
+
+        if ((String(size) == "medium")){amount += 3.00;}
+        else if (String(size) == "large"){amount += 5.00;}
+        else if (String(size) == "xlarge"){amount += 6.00;}
+        else if (String(size) == "4small"){amount += 19.00;}
+        else if (String(size) == "4medium"){amount += 29.00;}
+        else if (String(size) == "4large"){amount += 39.00;}  
+        else if (String(size) == "4xlarge"){amount += 43.00;}
+
+        totalCost += parseFloat(amount.toFixed(2));
+        localStorage.setItem("cost", totalCost);
+    }
 });

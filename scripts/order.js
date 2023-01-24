baf223944b23861a76a8d0a83fe29ef5229dbd79
ewanhughes26 <79140539+ewanhughes26@users.noreply.github.com>
2023-01-24 22:47:24 +0000

    Added display for number of items and cost

--- scripts/order.js
+++ scripts/order.js
@@ -1,6 +1,9 @@
 window.addEventListener("DOMContentLoaded", function(e)
 { 
     const url = window.location.href.replace("order.html", "complete.html");
+    var totalItems = parseInt(localStorage.getItem("items"));
+    var totalCost = parseFloat(localStorage.getItem("cost"));
+    displayValues();
     
     function clearBasket()
     {
@@ -12,5 +15,21 @@ window.addEventListener("DOMContentLoaded", function(e)
     function switchPage()
     {
         window.location.href = url;
+    }
+
+    function displayValues()
+    {
+        const cost = document.getElementById("cost");
+        const items = document.getElementById("items");
+
+        if (totalItems > 0)
+        {
+            items.innerText = ("Total items: " + totalItems);
+        }
+
+        if (totalCost > 0)
+        {
+            cost.innerText = ("Total cost: $" + totalCost);
+        }
     }
 });
